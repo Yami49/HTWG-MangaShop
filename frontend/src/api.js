b29741a1972afafdd8db7ea5a1af08d6bd18acc9
Georgios Gerontidis <georgiosgerontidis1@gmail.com>
2025-06-27 03:44:09 +0200

    fr

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,6 +2,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:1337', // URL deines Sails-Backends
+  baseURL: 'https://mangashop-backend.onrender.com', // URL deines Sails-Backends
   timeout: 10000,
 });

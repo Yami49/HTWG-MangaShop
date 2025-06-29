c60d2fd136886dba6fd9f2fb5113def41d283a4a
Georgios Gerontidis <georgiosgerontidis1@gmail.com>
2025-06-29 20:22:54 +0200

    bp

---
+++ backend/api/models/Bestellposition.js
@@ -0,0 +1,21 @@
+module.exports = {
+  attributes: {
+    bestellung: {
+      model: 'bestellung',
+      required: true
+    },
+    produkt: {
+      model: 'produkt',
+      required: true
+    },
+    menge: {
+      type: 'number',
+      required: true
+    },
+    einzelpreis: {
+      type: 'number',
+      columnType: 'float',
+      required: true
+    }
+  }
+}
